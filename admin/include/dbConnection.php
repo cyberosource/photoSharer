b218d6ebb72b1abce8fa25a5b15ebaecd079884e
cybero <cybero@me.com>
2017-07-28 19:34:23 +0100

    Commit prior to fixing

---
+++ admin/include/dbConnection.php
@@ -0,0 +1,13 @@
+<?php
+	ob_start();
+	session_start();
+//$con = mysqli_connect("localhost","my_user","my_password","my_db");
+//mysqli_connect(host,username,password,dbname,port,socket);
+	date_default_timezone_set(“Europe/London”);
+	$connection = mysqli_connect("localhost", "cyberome_photo", "ph0t05har3", "cyberome_xpertmon_photoshare") or die("Error In Connection.");
+/*
+u:	cyberome_photo
+p:	ph0t05har3
+
+*/
+?>	
